8751af7c5cc36cd4f421d4652281e4f8e5151f5a
Rohan <rohanpandey749@gmail.com>
2024-05-06 22:52:14 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Form from './components/Form';
-import Dashboard from './components/Dashboard';
-import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Form from "./components/Form";
+import Dashboard from "./components/Dashboard";
+import { useState } from "react";
 
 function App() {
-
-  const [details, setDetails] = useState({
-  })
+  const [details, setDetails] = useState({});
 
   const handleFormDetails = (val) => {
-    console.log("App component:", val)
-    setDetails(val)
-  }
+    console.log("App component:", val);
+    setDetails(val);
+  };
 
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={ <Form handleFormDetails={handleFormDetails}/> } />
-      <Route path='/prescription' element={<Dashboard details={details}/>} />
-    </Routes>
-    
-    </BrowserRouter>
-    
+    <Form />
+      {/* <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Form handleFormDetails={handleFormDetails} />}
+          />
+          <Route
+            path="/prescription"
+            element={<Dashboard details={details} />}
+          />
+        </Routes>
+      </BrowserRouter> */}
     </>
   );
 }
